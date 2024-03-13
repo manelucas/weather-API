a9f26be9259927f9e78d0c0fc6ec385255eaa2f5
manelucas <manelglucas@gmail.com>
2024-03-13 20:50:18 +0000

    added logic to fetch the weather api and basic data

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,18 +1,91 @@
 <template>
-        
-    <div class="row justify-content-center gap-3">
-        <div class="mt-5 weather-container">
-            <weather/>
-            <h1>Weather App</h1>
-            <div>
-                <input type="text" id="countryInput" placeholder="Enter country name">
-            </div>
-            <div>
-                <button id="weather-btn" class="btn btn-primary">Get Weather</button>
-            </div>
-            <div class="result-container">
-                <div id="weatherResult"></div>
-            </div>
+  <div class="row justify-content-center">
+    <div class="mt-5 weather-container">
+      <h1>Weather App</h1>
+      <input class="br-1" type="text" placeholder="Enter country" v-model="country">
+      
+      <button class="btn btn-primary btn-large px-5 py-2" @click="fetchWeather">Get Weather</button>
+      
+      <div class="result-container">
+        <div v-if="weatherData">
+          <div v-for="(weather, index) in weatherData" :key="index">
+            <h3>{{weather.name}}, {{weather.sys.country}}  </h3>
+            <span class="text-dark">{{ (weather.main.temp - 273.15).toFixed(0) }} °C</span>
+            <p>Weather: {{weather.weather[0].main}}</p>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      country: '',
+      apiKey: import.meta.env.VITE_APP_WEATHER_API_KEY,
+      weatherData: null,
+      error: null
+    };
+  },
+  methods: {
+    fetchWeather() {
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${this.country}&appid=${this.apiKey}`;
+
+      fetch(apiUrl)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(data => {
+          console.log(data);
+          this.weatherData = [data]; // Wrap single result in an array
+          this.error = null;
+        })
+        .catch(error => {
+          this.error = error.message;
+          console.error('There was a problem with the fetch operation:', error);
+        });
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.weather-container {
+    width: 500px;
+    height: 500px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 30px;
+}
+
+input {
+    padding: 15px;
+}
+
+.result-container {
+    border: 2px solid grey;
+    height: 400px;
+    width: 400px;
+    padding: 15px;
+}
+
+.br-1{
+  border-radius: 8px;
+}
+
+
+</style>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="../../css/app.css">
     <script src="https://kit.fontawesome.com/fafef7980b.js" crossorigin="anonymous"></script>
     <title>Weather API</title>
     @vite('resources/css/app.css')
